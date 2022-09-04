809db7cef1dc5bc72df125133f1c48a46f41473d
junhong91 <junhong199@gmail.com>
2022-09-04 22:33:52 +0900

    Apply Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { NAV_ROUTES } from "../config/routes.config";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar__wrapper">
+      <img
+        className="navbar__logo"
+        src={`${process.env.PUBLIC_URL}/Byredo_logo_wordmark.png`}
+        alt="logo"
+        onClick={() => navigate("/")}
+      />
+
+      <ul className="navbar__menu">
         {NAV_ROUTES.map((route) => (
-          <li key={route.path}>
+          <li className="navbar__menu__item" key={route.path}>
             <Link to={route.path} key={route.path}>
               {route.name}
             </Link>
